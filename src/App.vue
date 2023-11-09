249e536732ea6d7ff9a1451caad20e519367a454
datozhgenti <82455319+datozhgenti@users.noreply.github.com>
2023-11-09 23:25:30 +0400

    start portfolio page developing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,90 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <starterView></starterView>
 </template>
+
+<script setup>
+import starterView from "./views/starterView.vue";
+</script>
+
+<style>
+/* color variables */
+
+:root {
+  --page-background: #171717;
+  --text-starter-color: #ffffff;
+  --green-color: #27ae60;
+  --footer-text-color: #4f4f4f;
+}
+
+/* styles for all elements */
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: var(--text-starter-color);
+  font-family: "DMsans-regular", "sans-serif";
+}
+
+/* body styles */
+body {
+  background: var(--page-background);
+}
+
+/* font families */
+@font-face {
+  font-family: DMsans-regular;
+  src: url("@/fonts/DMSans-Regular.ttf");
+}
+
+@font-face {
+  font-family: DMsans-medium;
+  src: url("@/fonts/DMSans-Medium.ttf");
+}
+
+@font-face {
+  font-family: DMsans-bold;
+  src: url("@/fonts/DMSans-Bold.ttf");
+}
+
+/* font family classes */
+
+.font-regular {
+  font-family: "DMsans-regular", "sans-serif";
+}
+
+.font-medium {
+  font-family: "DMsans-medium", "sans-serif";
+}
+
+.font-bold {
+  font-family: "DMsans-bold", "sans-serif";
+}
+
+/* flex styles */
+
+.justify-center {
+  justify-content: center;
+}
+
+.align-center {
+  align-items: center;
+}
+
+/* displays */
+
+.flex {
+  display: flex;
+}
+
+.block {
+  display: block;
+}
+
+/* cursor styles */
+
+.pointer {
+  cursor: pointer;
+}
+</style>
---
+++ src/views/starterView.vue
@@ -0,0 +1,64 @@
+<template>
+  <main class="flex justify-center align-center">
+    <div class="flex">
+      <input
+        type="text"
+        placeholder="Enter Name, Please"
+        maxlength="13"
+        autofocus
+        :onkeydown="onInput"
+      />
+      <img
+        src="@/assets/arrow-icons/arrow-right.svg"
+        alt="go to portfolio button"
+        class="block pointer"
+      />
+    </div>
+  </main>
+</template>
+
+<script setup>
+function onInput(e) {
+  return /[a-z]/i.test(e.key);
+}
+</script>
+
+<style scoped>
+main {
+  height: 100vh;
+}
+
+input {
+  border: none;
+  background-color: transparent;
+  padding-bottom: 0.625rem;
+  border-bottom: 1px solid #4f4f4f;
+  font-size: 4rem;
+}
+
+input:focus {
+  outline: none;
+}
+
+img {
+  width: 5rem;
+  height: auto;
+}
+
+img,
+input {
+  border-bottom: 1px solid #4f4f4f;
+}
+
+@media (max-width: 48rem) {
+  input {
+    font-size: 5vw;
+    padding-bottom: 5px;
+  }
+
+  img {
+    width: 40px;
+    padding-bottom: 5px;
+  }
+}
+</style>
